22528491f77ad601d4d082869d05a1f8e71d431b
Swaminathan Vetri <svswaminathan@gmail.com>
2022-06-25 18:46:28 +0530

    Added comments for methods

--- library-demo/MyClasslib.cs
+++ library-demo/MyClasslib.cs
@@ -2,6 +2,7 @@
 namespace library_demo;
 public class MyClasslib
 {
+    //This method is used to print a message
     public void SayHello(string message)
     {
         Console.WriteLine($"Hello {message}");
